f8fc92bfcac3dbf44d87abd6789b8a461b9a6ba3
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-12-02 11:36:32 +0100

    Fix: Correct whitespace in user index view.

--- resources/views/manage/users/index.blade.php
+++ resources/views/manage/users/index.blade.php
@@ -5,7 +5,7 @@
                 <div class="col-md-12">
 
                 <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="col-lg-3 col-md-6 col-sm-12"> 
                     <div class="card">
                         <div class="card-body">
                             <div class="d-flex justify-content-between align-items-center">
