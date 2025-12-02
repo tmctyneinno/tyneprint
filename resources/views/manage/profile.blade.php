2af1a2171c3650abf81823353e220f1515de4962
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-12-02 11:49:32 +0100

    Refactor: Replace Form facade with standard HTML form in profile view
    The Form facade is being replaced with a standard HTML form for better maintainability and consistency.

--- resources/views/manage/profile.blade.php
+++ resources/views/manage/profile.blade.php
@@ -96,7 +96,7 @@
                            </div>
                         </div>
                         </div>
-                    {{Form::close()}}
+                    </form>
 
     </div>
                         </div>
